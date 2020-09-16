6d537c08a35468a7471841f802dca30b2fa824cf
deyad <deyadecember@gmail.com>
2020-09-16 20:09:19 +0300

    change logic according to task

--- src/ua/kpi/Main.java
+++ src/ua/kpi/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
             Model model = new Model(2,1,5);
-            model.simulate(1000);
+            model.simulate(1000, true);
 
+        for (int i = 0; i <10 ; i++) {
+            model = new Model(1 + Math.random() * 4, 1 + Math.random() * 6, (int) (1 + Math.random() * 9));
+            model.simulate(1000, false);
+        }
     }
 }
--- src/ua/kpi/Model.java
+++ src/ua/kpi/Model.java
@@ -4,19 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Model {
-    private double tnext;// найближча подыя
+    private double tnext;
     private double tcurr;
-    private double t0, // початок подыъ
-            t1; // кынець подыъ
+    private double t0,
+                   t1;
     private double delayCreate, delayProcess;
     private int numCreate, numProcess, failure;
     private int state, maxqueue, queue;
     private int nextEvent;
 
-//    private List<Double> waitingTimeList;
-//    private List<Double> serviceTimeList;
-//    private List<Integer> stateList;
-//    private double r;
+    private List<Double> waitingTimeList; // список для обчислення середнього часу очікування черзі
+    private List<Double> deltaTList;  // список для обчислення середнього   обчислення середнього завантаження пристрою
+    private List<Integer> lengthQueueList; // список довжин черг в момент tcurr
 
     public Model(double delay0, double delay1) {
         delayCreate = delay0;
@@ -26,9 +25,8 @@ public class Model {
         t0 = tcurr;
         t1 = Double.MAX_VALUE;
         maxqueue = 0;
-//        waitingTimeList = new ArrayList<>();
-//        serviceTimeList = new ArrayList<>();
-//        stateList = new ArrayList<>();
+        waitingTimeList = new ArrayList<>();
+        deltaTList = new ArrayList<>();
     }
 
     public Model(double delay0, double delay1, int maxQ) {
@@ -39,29 +37,27 @@ public class Model {
         t0 = tcurr;
         t1 = Double.MAX_VALUE;
         maxqueue = maxQ;
-//        waitingTimeList = new ArrayList<>();
-//        serviceTimeList = new ArrayList<>();
-//        stateList = new ArrayList<>();
-//        r = 0;
+        waitingTimeList = new ArrayList<>();
+        deltaTList = new ArrayList<>();
+        lengthQueueList = new ArrayList<>();
+
     }
 
-    public void simulate(double timeModeling) {
-        while (tcurr < timeModeling) { //	доки поточний час менший за час моделювання
+    public void simulate(double timeModeling, boolean flag) {
+        while (tcurr < timeModeling) {
 
-//            if (state == 1) {
-//                stateList.add(state);
-//                serviceTimeList.add(t1 - t0);
-//                this.waitingTimeList.add(t0 - tnext);
 
-//            }
             tnext = t0;
-            nextEvent = 0;
+            nextEvent = 0; // надходження заявки
 
-            if (t1 < tnext) { //3)	знайти найменший із моментів часу «момент надходження вимоги у систему»
-                //// та «момент звільнення пристрою обслуговування» і запам’ятати, якій події  він відповідає;
+            if (t1 < tnext) {
                 tnext = t1;
                 nextEvent = 1;
             }
+
+            waitingTimeList.add((tnext - tcurr) * queue);
+            deltaTList.add((tnext - tcurr) * state);
+
             tcurr = tnext;
             switch (nextEvent) {
                 case 0:
@@ -71,30 +67,31 @@ public class Model {
                     event1();
                     break;
             }
-            printInfo();
-
+            if (flag)
+                printInfo();
+            lengthQueueList.add(state);
         }
-        printStatistic();
-//        System.out.println("Average waiting time= " + getAverageWaitingTime());
-//        System.out.println("Average service time= " + getAverageServiceTime());
-//        System.out.println("Average load= " + getSumWServiceTime(timeModeling));
+        if (flag)
+            System.out.println("-----------------------------------------------VERIFICATION---------------------------------------------------");
+        printStatistic(timeModeling);
     }
 
-    public void printStatistic() {
-        System.out.println(" numCreate= " + numCreate + " numProcess = " + numProcess + " failure = " + failure);
+    public void printStatistic(double timeModeling) {
+        System.out.printf(" numCreate= %3d;  numProcess = %3d;  failure = %3d;  average waiting time=%5.10f;  Average device=%5.10f;  probability of failure = %5.10f;  average queue length = %5.10f;\n",
+                numCreate, numProcess, failure, getSumWaitingTime() / numProcess, getSumDeltaT() / timeModeling, getProbabilityOfFailure(), getSumWaitingTime() / timeModeling);
     }
 
     public void printInfo() {
         System.out.println(" t= " + tcurr + " state = " + state + " queue = " + queue);
     }
 
-    public void event0() {
+    public void event0() { // start service
         t0 = tcurr + getDelayOfCreate();
         numCreate++;
+
         if (state == 0) {
             state = 1;
             t1 = tcurr + getDelayOfProcess();
-
         } else {
             if (queue < maxqueue)
                 queue++;
@@ -103,7 +100,8 @@ public class Model {
         }
     }
 
-    public void event1() {
+    public void event1() { // end of service
+
         t1 = Double.MAX_VALUE;
         state = 0;
         if (queue > 0) {
@@ -112,8 +110,6 @@ public class Model {
             t1 = tcurr + getDelayOfProcess();
         }
         numProcess++;
-
-
     }
 
 
@@ -125,20 +121,16 @@ public class Model {
         return FunRand.Exp(delayProcess);
     }
 
-//    private Double getAverageWaitingTime() {
-//        return waitingTimeList.stream().mapToDouble(i -> i).average().getAsDouble();
-//    }
-//
-//    private Double getAverageServiceTime() {
-//        System.out.println(serviceTimeList);
-//        return serviceTimeList.stream().mapToDouble(i -> i).average().getAsDouble();
-//    }
-//
-//    private Double getSumWServiceTime(double timeModeling) {
-//        double number = 0;
-//        for (int i = 0; i < serviceTimeList.size() - 1; i++) {
-//            number += serviceTimeList.get(i) * stateList.get(i);
-//        }
-//        return number / timeModeling;
-//    }
+    private Double getSumWaitingTime() {
+        return waitingTimeList.stream().mapToDouble(i -> i).sum();
+    }
+
+    private Double getSumDeltaT() {
+        return deltaTList.stream().mapToDouble(i -> i).sum();
+    }
+
+    private double getProbabilityOfFailure() {
+        return (double) failure / numCreate;
+    }
+
 }
